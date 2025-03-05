f63ad3aa6ac14c24401e935cbddd34a2512430db
Ciprian <ciprian@mytime.com>
2025-03-05 14:31:47 +0200

    add CRUD APIs for columns and stories

--- app/controllers/columns_controller.rb
+++ app/controllers/columns_controller.rb
@@ -1,2 +1,30 @@
 class ColumnsController < ApplicationController
+    def index
+        render json: Column.all
+    end
+
+    def show
+        render json: Column.find(params[:id])
+    end
+
+    def create
+        column = Column.create(column_params)
+        render json: column, status: :created
+    end
+
+    def update
+        column = Column.find(params[:id])
+        column.update!(column_params)
+        render json: column
+    end
+
+    def destroy
+        Column.find(params[:id]).destroy
+        head :no_content
+    end
+
+    private
+    def column_params
+        params.permit(:title)
+    end
 end
--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,2 +1,30 @@
 class StoriesController < ApplicationController
+    def index
+        render json: Story.all
+    end
+
+    def show
+        render json: Story.find(params[:id])
+    end
+
+    def create
+        story = Story.create(story_params)
+        render json: story, status: :created
+    end
+
+    def update
+        story = Story.find(params[:id])
+        story.update!(story_params)
+        render json: story
+    end
+
+    def destroy
+        Story.find(params[:id]).destroy
+        head :no_content
+    end
+
+    private
+    def story_params
+        params.permit(:title)
+    end
 end
