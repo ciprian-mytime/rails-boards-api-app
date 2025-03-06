f5b851ec203bae1e3c4971227b71b015988469cd
Ciprian <ciprian@mytime.com>
2025-03-06 13:46:40 +0200

    finish crud for columns

--- app/controllers/columns_controller.rb
+++ app/controllers/columns_controller.rb
@@ -1,15 +1,33 @@
 class ColumnsController < ActionController::Base
     before_action :set_board
-    before_action :set_column
+    before_action :set_column, only: [:edit, :update, :destroy]
 
+    def new
+        @column = @board.columns.new
+      end
+    def create
+        @column = @board.columns.new(column_params)
+
+        if @column.save
+            redirect_to @board, notice: "Column created sucessfully"
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+    
     def edit
     end
     def update
         if @column.update(column_params)
-            redirect_to board_path(@column.board), notice: "column updated successfully", status: :see_other
+            redirect_to board_path(@column.board), notice: "Column updated successfully", status: :see_other
         else
             render :edit, status: :unprocessable_entity
         end
+    end
+
+    def destroy
+        @column.destroy
+        redirect_to board_path(@board), notice: "Column destroyed successfully", status: :see_other
     end
 
     private
