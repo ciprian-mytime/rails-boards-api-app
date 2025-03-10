e33978c495959e189fae5b363626d6c90698aac2
Ciprian <ciprian@mytime.com>
2025-03-10 16:03:29 +0200

    code formatting

--- spec/requests/board_spec.rb
+++ spec/requests/board_spec.rb
@@ -1,26 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Board, type: :request do
-    let(:user) { FactoryBot.create(:user) }
-    let(:boards) {[
-      FactoryBot.build(:board, id: 1),
-      FactoryBot.build(:board, id: 2),
-      FactoryBot.build(:board, id: 3)
-    ]}
-    let(:board) { boards.first }
+  let(:user) { FactoryBot.create(:user) }
+  let(:boards) {[
+    FactoryBot.build(:board, id: 1),
+    FactoryBot.build(:board, id: 2),
+    FactoryBot.build(:board, id: 3)
+  ]}
+  let(:board) { boards.first }
 
-    before do
-      sign_in user, scope: :user
-      allow_any_instance_of(BoardsController).to receive(:authorize).and_return(true)
-      allow_any_instance_of(BoardsController).to receive(:policy_scope).and_return(boards)
+  before do
+    sign_in user, scope: :user
+    allow_any_instance_of(BoardsController).to receive(:authorize).and_return(true)
+    allow_any_instance_of(BoardsController).to receive(:policy_scope).and_return(boards)
+  end
+
+  describe "GET /boards" do
+    it "returns all boards" do
+      allow(Board).to receive(:all).and_return(boards)
+
+      get "/boards"
+      expect(response).to render_template :index
     end
-
-    describe "GET /boards" do
-      it "returns all boards" do
-        allow(Board).to receive(:all).and_return(boards)
-
-        get "/boards"
-        expect(response).to render_template :index
-      end
-    end
+  end
 end
