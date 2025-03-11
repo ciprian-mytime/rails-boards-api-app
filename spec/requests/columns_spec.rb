80c2c79d13b1d78358c0158d46151bc4b677c949
Ciprian <ciprian@mytime.com>
2025-03-11 13:40:42 +0200

    add swagger for api calls

---
+++ spec/requests/columns_spec.rb
@@ -0,0 +1,151 @@
+require 'swagger_helper'
+
+RSpec.describe 'columns', type: :request do
+
+  path '/boards/{board_id}/columns' do
+    # You'll want to customize the parameter types...
+    parameter name: 'board_id', in: :path, type: :string, description: 'board_id'
+
+    get('list columns') do
+      response(200, 'successful') do
+        let(:board_id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    post('create column') do
+      response(200, 'successful') do
+        let(:board_id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
+  path '/boards/{board_id}/columns/new' do
+    # You'll want to customize the parameter types...
+    parameter name: 'board_id', in: :path, type: :string, description: 'board_id'
+
+    get('new column') do
+      response(200, 'successful') do
+        let(:board_id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
+  path '/boards/{board_id}/columns/{id}/edit' do
+    # You'll want to customize the parameter types...
+    parameter name: 'board_id', in: :path, type: :string, description: 'board_id'
+    parameter name: 'id', in: :path, type: :string, description: 'id'
+
+    get('edit column') do
+      response(200, 'successful') do
+        let(:board_id) { '123' }
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
+  path '/boards/{board_id}/columns/{id}' do
+    # You'll want to customize the parameter types...
+    parameter name: 'board_id', in: :path, type: :string, description: 'board_id'
+    parameter name: 'id', in: :path, type: :string, description: 'id'
+
+    get('show column') do
+      response(200, 'successful') do
+        let(:board_id) { '123' }
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    patch('update column') do
+      response(200, 'successful') do
+        let(:board_id) { '123' }
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    put('update column') do
+      response(200, 'successful') do
+        let(:board_id) { '123' }
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    delete('delete column') do
+      response(200, 'successful') do
+        let(:board_id) { '123' }
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+end
